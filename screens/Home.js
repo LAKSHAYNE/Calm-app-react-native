da6213809465848b57b7d9e6d18606bfe9c65d3b
LAKSHAY SHARMA <lakshayne@gmail.com>
2021-07-10 13:15:32 +0530

    added gradient and scroll animation

--- screens/Home.js
+++ screens/Home.js
@@ -1,59 +1,190 @@
-import React from 'react'
-import { Image, ImageBackground, Platform, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
-import MainTile from '../components/mainTile'
-import SquareTile from '../components/SquareTile'
-import LongTile from './LongTile'
-
+import React, { useState } from "react";
+import {
+  Image,
+  ImageBackground,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import MainTile from "../components/mainTile";
+import SquareTile from "../components/SquareTile";
+import LongTile from "./LongTile";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Home = () => {
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-        <StatusBar style="light" />
-            <ImageBackground 
-            source={{uri:"https://res.cloudinary.com/calm-com/image/upload/v1582139698/jasper-lake.jpg"}} 
-            style={styles.backgroundImage}>
-                <Image
-                    style={styles.logo}
-                    source={{uri:"https://www.calm.com/_n/images/calm-logo.png"}}
-                />
-            <ScrollView showsHorizontalScrollIndicator={Platform.OS=="web"?false:true} removeClippedSubviews={true} style={{position:"relative",zIndex:3, height:10}}   contentContainerStyle={{paddingTop:25,alignItems:"center",justifyContent:"center"}}>
-                <View style={{height:300}} />
-                <MainTile/>
-                <SquareTile/>
-                <SquareTile/>
-                <SquareTile/>
-                <LongTile/>
-                <LongTile/>
-                <LongTile/>
+  const [opaci, setopaci] = useState(1);
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+      <StatusBar style="light" />
+      <LinearGradient
+        style={{ flex: 1 }}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+        colors={["#60B1E7","#6462E0"]}
+      >
+        <ImageBackground
+          imageStyle={{ opacity: opaci }}
+          source={{
+            uri: "https://res.cloudinary.com/calm-com/image/upload/v1582139698/jasper-lake.jpg",
+          }}
+          style={styles.backgroundImage}
+        >
+          <Image
+            style={styles.logo}
+            source={{ uri: "https://www.calm.com/_n/images/calm-logo.png" }}
+          />
+          <ScrollView
+            onScroll={(e) => {
+              Platform.OS == "web"
+                ? "auto"
+                : setopaci(
+                    opaci >= 0
+                      ? 1 - e.nativeEvent.contentOffset.y * 0.003030303030303
+                      : 0
+                  );
+            }}
+            showsHorizontalScrollIndicator={Platform.OS == "web" ? false : true}
+            removeClippedSubviews={true}
+            style={{ position: "relative", zIndex: 3, height: 10 }}
+            contentContainerStyle={{
+              paddingTop: 25,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <View style={{ height: 300 }} />
+            <MainTile />
+            <SquareTile />
+            <SquareTile />
+            <SquareTile />
+            <View
+              style={{
+                marginTop: 20,
+                width: Platform.OS == "web" ? 800 : 330,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ color: "white", fontSize: 17 }}>
+                Quick and Easy
+              </Text>
+              <Text style={{ color: "white", fontSize: 17 }}>
+                {"See All >"}
+              </Text>
+            </View>
+            <ScrollView
+              showsHorizontalScrollIndicator={
+                Platform.OS == "web" ? false : true
+              }
+              style={{ width: Platform.OS == "web" ? 800 : "auto" }}
+              horizontal={true}
+            >
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
             </ScrollView>
-            </ImageBackground>
+            <View
+              style={{
+                marginTop: 20,
+                width: Platform.OS == "web" ? 800 : 330,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ color: "white", fontSize: 17 }}>
+                Eating Stress
+              </Text>
+              <Text style={{ color: "white", fontSize: 17 }}>
+                {"See All >"}
+              </Text>
+            </View>
+            <ScrollView
+              showsHorizontalScrollIndicator={
+                Platform.OS == "web" ? false : true
+              }
+              style={{ width: Platform.OS == "web" ? 800 : "auto" }}
+              horizontal={true}
+            >
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+            </ScrollView>
+            <View
+              style={{
+                marginTop: 20,
+                width: Platform.OS == "web" ? 800 : 330,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ color: "white", fontSize: 17 }}>
+                Meditation for Inner Peace
+              </Text>
+              <Text style={{ color: "white", fontSize: 17 }}>
+                {"See All >"}
+              </Text>
+            </View>
+            <ScrollView
+              showsHorizontalScrollIndicator={
+                Platform.OS == "web" ? false : true
+              }
+              style={{ width: Platform.OS == "web" ? 800 : "auto" }}
+              horizontal={true}
+            >
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+              <LongTile />
+            </ScrollView>
+          </ScrollView>
+        </ImageBackground>
+      </LinearGradient>
+    </SafeAreaView>
+  );
+};
 
-        </SafeAreaView>
-    )
-}
-
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        backgroundColor:"black",
-    },
-    backgroundImage:{
-        position: "relative",
-        opacity: 0.8,
-        flex: 1,
-        resizeMode:"cover",
-        justifyContent:"center",
-        alignItems:"center"
-    },
-    logo:{
-        height: 100,
-        width: 100,
-        resizeMode:"contain",
-        zIndex:0,
-        position: "absolute",
-        top:100
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+  backgroundImage: {
+    position: "relative",
+    opacity: 0.8,
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    height: 100,
+    width: 100,
+    resizeMode: "contain",
+    zIndex: 0,
+    position: "absolute",
+    top: 100,
+  },
+});
--- screens/LongTile.js
+++ screens/LongTile.js
@@ -1,50 +1,26 @@
-import React from "react";
-import { Dimensions, Image, ImageBackground, Platform, StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 const LongTile = () => {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        imageStyle={{borderRadius:15,resizeMode:"cover"}}
-        style={styles.tileImg}
-        source={{ uri: "https://media-cdn.tripadvisor.com/media/photo-s/05/b8/4b/b7/tanta-moon-luxury-villas.jpg" }}
-      >
-        <View style={styles.textCon}>
-          <Text style={styles.tileTexth}>Daily Calm</Text>
-          <Text style={styles.tileTextp}>Josh D.-Patience</Text>
-        </View>
-      </ImageBackground>
+      <Image style={styles.img} source={{uri:"https://www.teahub.io/photos/full/274-2742530_night-snow-wallpaper-hd.jpg"}} />
+      <Text style={{fontSize:20,color:"white",fontWeight:"bold"}}>Calming Anxiety</Text>
+      <Text style={{color:"white"}}>Meditation - Tamara Levitt</Text>
     </View>
-  );
-};
+  )
+}
 
-export default LongTile;
-Platform.OS
+export default LongTile
+
 const styles = StyleSheet.create({
-  container: {
-    margin:10,
-    height: 100,
-    width: Platform.OS=="web"?400:330,
+  container:{
+    margin: 10
   },
-  tileImg: {
-    flexDirection:"row-reverse",
-    height: "100%",
-    borderRadius:20,
-  },
-  tileTextp:{
-    fontWeight:"bold",
-    fontSize:15,
-  },
-  tileTexth:{
-    fontWeight:"bold",
-    fontSize:35,
-  },
-  textCon:{ 
-    borderTopRightRadius:15,
-    borderBottomRightRadius:15,
-    alignItems:"center",
-    justifyContent:"center",
-    flex:0.7,
-    backgroundColor:"white",
-  opacity:0.7}
-});
+  img:{
+    height: 270,
+    width: 200,
+    resizeMode:"cover",
+    borderRadius:20
+  }
+})
